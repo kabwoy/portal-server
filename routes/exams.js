80c88926c00d949962d0434d54a153147e8b6fea
kabwoy <deejaykabwoy@gmail.com>
2022-12-16 13:21:54 +0300

    with exam module

--- routes/exams.js
+++ routes/exams.js
@@ -3,13 +3,30 @@ const Exam = require("../models/Exam")
 const router = express.Router()
 
 router.get("/exams" , async(req,res)=>{
-    const exams = await Exam.findAll({attributes:{exclude:['createdAt' , 'updatedAt' , 'type_of_exam' , 'studentId']}})
+    const exams = await Exam.findAll({attributes:{exclude:['createdAt' , 'updatedAt']}})
     res.status(200).json(exams)
 })
+
 router.post("/exams" , async(req,res)=>{
     Exam.create(req.body).then(()=>{
         res.status(201).json({message:"Exam details Added"})
     }).catch(err=>res.status(400).json(err))
+})
+router.get("/exams/:id" , async(req,res)=>{
+    const exam = await Exam.findByPk(req.params.id, {attributes:{exclude:['id' , 'updatedAt' ,  'createdAt' , 'studentId' , 'type_of_exam']}})
+    res.status(200).json(exam)
+})
+
+router.get("/exams/:id/edit" , async(req,res)=>{
+
+    const exam = await Exam.findByPk(req.params.id)
+    res.status(200).json(exam)
+})
+
+router.patch("/exams/:id" , async(req,res)=>{
+    const exam =  Exam.update(req.body , {where:{id:req.params.id}}).then(()=>{
+        res.status(201).json({message:"Data updated successfully"})
+    }).catch((err)=>res.status(403).json(err.message))
 })
 
 module.exports = router
